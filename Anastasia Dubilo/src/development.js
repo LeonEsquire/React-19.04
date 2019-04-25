f33094092c6a960c4309a21e0785e2611575636f
NastasyaLennon <ETGwerrgo13>
2019-04-25 21:58:22 +0300

    lesson2

---
+++ Anastasia Dubilo/src/development.js
@@ -0,0 +1,6 @@
+export default class Development {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ Anastasia Dubilo/src/index.js
@@ -0,0 +1,4 @@
+import development from './development'
+
+let dev1 = new development('name', 20);
+console.log(dev1);
