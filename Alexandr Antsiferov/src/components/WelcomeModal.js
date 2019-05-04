32025acb6659b2b46ccb82716df76b80b111a526
MarrySad <mvrsad@gmail.com>
2019-05-04 16:40:08 +0700

    начало работы, создание компонетов

---
+++ Alexandr Antsiferov/src/components/WelcomeModal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WelcomeModal extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default WelcomeModal;
