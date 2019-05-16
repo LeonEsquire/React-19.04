2eb577e45655f9f8b653bd4f08fb7a9581234c82
MarrySad <mvrsad@gmail.com>
2019-05-16 20:12:44 +0700

    добавил комменты

--- Alexandr Antsiferov/lesson5/src/app/components/Comment.js
+++ Alexandr Antsiferov/lesson5/src/app/components/Comment.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
 
-class Post extends Component {
+class Comment extends Component {
   render() {
     return (
       <div className="card border-secondary mb-3">
         <div className="card-header">
-          <Link to={`/posts/${this.props.id}`}>{this.props.title}</Link>
+          <Link to={`/comments/${this.props.id}`}>{this.props.name}</Link>
         </div>
         <div className="card-body text-secondary">
           <p>{this.props.body}</p>
+          <p>{this.props.email}</p>
         </div>
       </div>
     );
   }
 }
 
-export default Post;
+export default Comment;
