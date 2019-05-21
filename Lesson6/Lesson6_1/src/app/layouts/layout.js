5fc23fd12d27198e614b9a8984f7630f03f6376f
MarrySad <mvrsad@gmail.com>
2019-05-21 17:34:47 +0700

    fix

--- Lesson6/Lesson6_1/src/app/layouts/layout.js
+++ Lesson6/Lesson6_1/src/app/layouts/layout.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React from 'react';
 
-export default class layout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.brand = "React blog!";
-  }
+export default class layout extends React.Component
+{
 
-  isActive(
-    href // вычисление активного пункта меню
-  ) {
-    return window.location.pathname === href;
-  }
+	constructor(props)
+	{
+		super(props);
+		this.brand = 'React blog!';
 
-  render() {
-    return (
-      <div>
-        <Menu brand={this.brand}>
-          <MenuItem href="/" active={this.isActive("/")}>
-            Главная
-          </MenuItem>
-          <MenuItem href="/users" active={this.isActive("/users")}>
-            Пользователи
-          </MenuItem>
-        </Menu>
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12">{this.props.children}</div>
-          </div>
-        </div>
-        <footer className="card-footer">&copy; 2018</footer>
-      </div>
-    );
-  }
+	}
+
+	isActive(href) // вычисление активного пункта меню
+	{
+		return window.location.pathname === href;
+	}
+
+	render()
+	{	
+		return (
+			<div>
+				<Menu brand={this.brand}>
+					<MenuItem href="/" active={this.isActive('/')}>
+						Главная
+					</MenuItem>
+					<MenuItem href="/users" active={this.isActive('/users')}>
+						Пользователи
+					</MenuItem>
+				</Menu>
+				<div className="container">
+					<div className="row">
+						<div className="col-xs-12">
+							{this.props.children}						
+						</div>
+					</div>
+				</div>	
+				<footer className="card-footer">
+					&copy; 2018
+				</footer>
+			</div>
+		);
+	}
 }
