e4bd9a394f338d0d8772b7f66fffaab6b419e7f7
MarrySad <mvrsad@gmail.com>
2019-05-28 13:28:59 +0700

    Update Alexandr Antsiferov/lesson7/src/app/redusers/userReduser.js
    Co-Authored-By: LeonEsquire <leva2004@yandex.ru>

--- Alexandr Antsiferov/lesson7/src/app/redusers/userReduser.js
+++ Alexandr Antsiferov/lesson7/src/app/redusers/userReduser.js
@@ -7,7 +7,7 @@ export default function reducer(
     case "FETCH_USER_FULFILLED": {
       return { ...state, user: action.payload };
     }
-    case "RESET_USER_FULFILLED": {
+    case "RESET_USER": {
       return { ...state, user: {} };
     }
   }
