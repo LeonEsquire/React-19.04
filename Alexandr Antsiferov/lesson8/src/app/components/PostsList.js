57fca1295dc6f8a0919ec08d10da35755a64fbf2
MarrySad <mvrsad@gmail.com>
2019-05-28 13:31:03 +0700

    Update Alexandr Antsiferov/lesson8/src/app/components/PostsList.js
    Co-Authored-By: LeonEsquire <leva2004@yandex.ru>

--- Alexandr Antsiferov/lesson8/src/app/components/PostsList.js
+++ Alexandr Antsiferov/lesson8/src/app/components/PostsList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Post from "./Post";
 
 const PostsList = props => {
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios
